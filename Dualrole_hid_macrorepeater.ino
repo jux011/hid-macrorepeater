a6c2d820d20c681cf588c884ca5a371523ed6ad7
jux011 <jackjxie.whs@gmail.com>
2025-08-30 15:39:15 -0700

    implement switch 3 ON sets delay to 15ms OFF uses saved delay

--- Dualrole_hid_macrorepeater.ino
+++ Dualrole_hid_macrorepeater.ino
@@ -57,6 +57,10 @@ bool should_power_led_on(bool *booleanStates) {
   return booleanStates[1] == HIGH && macro_len < MACRO_BUFFER_SIZE;
 }
 
+bool should_delay(bool *booleanStates) {
+  return booleanStates[3];
+}
+
 //------------- Core0 -------------//
 void setup() {
   Serial.begin(115200);
@@ -111,7 +115,7 @@ void loop() {
         // hardcoded LED ON
         led_state = true;
         digitalWrite(PIN_SWITCH1_LED, HIGH);
-        play_macro(); // this is a delay() blocking call
+        play_macro();  // this is a delay() blocking call
         // if ()
       } else {
         Serial.println("Button pressed, but macro is empty.");
@@ -169,11 +173,18 @@ void loop1() {
 void play_macro() {
   while (!usb_hid.ready()) { yield(); }
   usb_hid.sendReport(0, &macroBuffer[0], sizeof(hid_keyboard_report_t));
-  for (size_t i = 1; i < macro_len; ++i) {
-    delay(delayBuffer[i]);
-    while (!usb_hid.ready()) { yield(); }
-    // delay(15);  // 66hz, 400wpm
-    usb_hid.sendReport(0, &macroBuffer[i], sizeof(hid_keyboard_report_t));
+  if (should_delay(switchStates)) {
+    for (size_t i = 1; i < macro_len; ++i) {
+      delay(delayBuffer[i]);
+      while (!usb_hid.ready()) { yield(); }
+      usb_hid.sendReport(0, &macroBuffer[i], sizeof(hid_keyboard_report_t));
+    }
+  } else {
+    for (size_t i = 1; i < macro_len; ++i) {
+      delay(15);  // 66hz, 400wpm
+      while (!usb_hid.ready()) { yield(); }
+      usb_hid.sendReport(0, &macroBuffer[i], sizeof(hid_keyboard_report_t));
+    }
   }
   const uint8_t null_report[8] = { 0 };
   while (!usb_hid.ready()) { yield(); }
@@ -201,8 +212,7 @@ void save_to_macro(const hid_keyboard_report_t *report) {
     delayBuffer[0] = now;
     macro_len = 1;
     Serial.printf("Macro started, macro starttime: %u\r\n", delayBuffer[0]);
-  }
-  else if (macro_len < MACRO_BUFFER_SIZE) {
+  } else if (macro_len < MACRO_BUFFER_SIZE) {
     macroBuffer[macro_len] = *report;
     delayBuffer[macro_len] = now - delayBuffer[0];
     delayBuffer[0] = now;
